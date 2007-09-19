a72a48c6e541665c2ae36520f78e7f30c6d72312
Tom Grennan <tgrennan@vyatta.com>
2007-09-19 14:19:55 -0700

    fix linda/lintian detected errors

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the vyatta-op package
-#
-0 4	* * *	root	vyatta-op_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(vyatta-op):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="vyatta-op" command="/usr/bin/vyatta-op"
