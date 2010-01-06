a5d04a5c46c1e52d14a228343c8418167f5c801e
Stephen Hemminger <stephen.hemminger@vyatta.com>
2010-01-06 08:20:12 -0800

    Simple SNMP operational command
    show snmp
    Wrapper around snmpstatus, simple way to check if server is up

---
+++ scripts/vyatta-show-snmp.pl
@@ -0,0 +1,49 @@
+#! /usr/bin/perl
+
+# **** License ****
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# This code was originally developed by Vyatta, Inc.
+# Portions created by Vyatta are Copyright (C) 2007 Vyatta, Inc.
+# All Rights Reserved.
+# 
+# Author: Stephen Hemminger
+# Date: January 2010
+# Description: Script to display snmp information
+# 
+# **** End License ****
+#
+use strict;
+use warnings;
+
+sub get_community {
+    my $snmpcfg = '/etc/snmp/snmpd.conf';
+
+    open (my $cfg, '<', $snmpcfg)
+	or return;
+    my $community;
+    while (<$cfg>) {
+	next unless m/^r[ow]community (\w+)$/;
+	$community = $1;
+	last;
+    }
+    close $cfg;
+    return $community;
+}
+
+my $community = get_community();
+die "No SNMP community's configured\n"
+    unless $community;
+
+exec 'snmpstatus', '-c', $community, '-v', '1', 'localhost'
+    or die "Can't exec snmpstatus: $!";
+
+
+
